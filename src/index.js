00f5984cdb0cedaa9a08d83389737b8cf4ead427
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-11-04 10:38:28 +0000

    added js interactivity to truth lie component, created footer component and changed responsive styles

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 // form controls
-const bookRes = document.querySelector("#bookRes").value;
+const bookRes = document.querySelector("#bookRes");
 const formBtn = document.querySelector("#form-submit");
 const response = document.querySelector("#response");
 
 function send() {
-  alert(bookRes);
+  if (!bookRes.value) return;
+  alert("that's a good choice!");
 }
 formBtn.addEventListener("click", send);
 
-// 2 truth 1 lie logic
+// 2 truth 1 lie logic (very messy) make into class?
 
 const subtitle = document.querySelector(".truth-lie");
 const randomSubtitleArr = ["I love code", "I can fly", "I'm a nerd"];
+const backBtn = document.querySelector("#back-button");
+const forwardBtn = document.querySelector("#forward-button");
 
 document.addEventListener("DOMContentLoaded", () => {
-  window.setInterval(() => {
-    subtitle.innerHTML =
-      randomSubtitleArr[Math.floor(Math.random() * randomSubtitleArr.length)];
-  }, 3000);
+  let step = 0;
+
+  forwardBtn.addEventListener("click", () => {
+    if (step >= randomSubtitleArr.length - 1) {
+      console.log("was too high, reset step");
+      step = 0;
+      return (subtitle.innerHTML = randomSubtitleArr[step]);
+    }
+    step += 1;
+
+    return (subtitle.innerHTML = randomSubtitleArr[step]);
+  });
+
+  backBtn.addEventListener("click", () => {
+    if (step <= 0) {
+      step = randomSubtitleArr.length - 1;
+      return (subtitle.innerHTML = randomSubtitleArr[step]);
+    }
+    step -= 1;
+    return (subtitle.innerHTML = randomSubtitleArr[step]);
+  });
 });
